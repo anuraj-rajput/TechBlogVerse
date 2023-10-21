f1a1085ad28cdabc81dbae3fbd9c85ca2dba75d2
Anuraj Rajput <rajputanuraj1@gmail.com>
2023-10-22 00:42:54 +0530

    initial commit after appwrite config

---
+++ TechBlogVerse/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import configurations from '../conf/config'
+
+import { Client, Account,ID } from 'appwrite';
+
+export class AuthService{
+
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(configurations.appwriteUrl)
+            .setProject(configurations.appwriteProjectId);
+        // Replace with your project ID
+        this.account = new Account(this.client);     
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method
+                return this.login(email, password)
+            } else {
+                return userAccount
+            }
+        
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login(email, password) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service ::getCurrentUser::error", error)
+        
+        }
+        return null;
+    }
+
+    async logout() { 
+        try {
+            return await this.account.deleteSessions();
+                    
+        } catch (error) {
+            throw error
+            
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default authService;
